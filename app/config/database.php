6e7b4b9358a1b05ec5bef7c036b0f4472183bef1
vnino2003 <vincent.edu.mail@gmail.com>
2025-09-13 17:37:44 +0800

    databaseAdd2

--- app/config/database.php
+++ app/config/database.php
@@ -59,7 +59,7 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
     'username'	=> 'sql12798473',
     'password'	=> 'bEMfhxHF8l',
